6abbb87b1ffca7704f21da2b454d82de19ae08ee
tomiris.rakhymzhan <tomiris.rakhymzhan@gmail.com>
2024-07-23 16:03:58 +0500

    logout screen swipe down added

--- Ozinshe/Controllers/Profile/LogoutViewController.swift
+++ Ozinshe/Controllers/Profile/LogoutViewController.swift
@@ -23,6 +23,8 @@ class LogoutViewController: UIViewController, UIGestureRecognizerDelegate, Langu
     
     @IBOutlet weak var backgroundView: UIView!
     
+    var viewTranslation = CGPoint(x: 0, y: 0)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         configureLayers()
@@ -30,6 +32,9 @@ class LogoutViewController: UIViewController, UIGestureRecognizerDelegate, Langu
         let tap = UITapGestureRecognizer(target: self, action: #selector(dismissView))
         tap.delegate = self
         view.addGestureRecognizer(tap)
+        
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handleDismiss)))
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,6 +60,26 @@ class LogoutViewController: UIViewController, UIGestureRecognizerDelegate, Langu
         yesButton.setTitle("YES_BUTTON".localized(), for: .normal)
         noButton.setTitle("NO_BUTTON".localized(), for: .normal)
         subTitleLabel.text = "CONFIRMATION".localized()
+    }
+    
+    @objc func handleDismiss(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+            case .changed:
+                viewTranslation = sender.translation(in: view)
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.backgroundView.transform = CGAffineTransform(translationX: 0, y: self.viewTranslation.y)
+                })
+            case .ended:
+                if viewTranslation.y < 100 {
+                    UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                        self.backgroundView.transform = .identity
+                    })
+                } else {
+                    dismiss(animated: true, completion: nil)
+                }
+            default:
+                break
+        }
     }
     
     @objc func dismissView(){
